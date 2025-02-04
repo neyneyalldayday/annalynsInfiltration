a2e629c16d7ddd8d4905d5bb025ac20bff680b91
neyneyalldayday <drfrankenstein123@gmail.com>
2025-02-04 12:45:18 -0600

    fixed edge cases

--- assets/script.js
+++ assets/script.js
@@ -1,55 +1,3 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 document.querySelector("#refresh").addEventListener("click", () => {
     document.location.reload()
 })
@@ -69,5 +17,46 @@ document.addEventListener('DOMContentLoaded', function() {
         window[variableName] = boolArray[i];
     }
     
-    console.log("knightAwake ->",knightIsAwake, "archerAwake ->", archerIsAwake, "prisonerAwake ->",prisonerIsAwake, "petdog ->", petDogIspresent);
+    console.log("knightAwake ->",knightIsAwake, "archerAwake ->", archerIsAwake, "prisonerAwake ->",prisonerIsAwake, "petdog ->", petDogIspresent); 
+
+
+    const canExecuteFastAttack = () => {
+                knightIsAwake? console.log("annalyn smacked that fool") : console.log("annalyn cant smack that fool");
+       }
+       
+    const canSpy = () => {
+        if (!knightIsAwake && !archerIsAwake && !prisonerIsAwake ) {
+            console.log("annalyn can spy on the situation")
+        } else if (knightIsAwake && archerIsAwake && prisonerIsAwake) {
+            console.log("dont spy right now foo")
+        } else if (!knightIsAwake && !archerIsAwake && prisonerIsAwake ) {
+            console.log("be careful")
+        } else {
+            console.log("some buster is awake")
+        }
+        
+    } 
+
+    const canSignalPrisoner = () => {
+        if (!archerIsAwake && prisonerIsAwake) {
+            console.log("annalyn can signal her homie")
+        } else  {
+            console.log("annalyn will get caught if she tries to signal her homie")
+        }
+        
+    }
+
+    const canFreePrisoner = () => {
+        if (!knightIsAwake && !archerIsAwake && petDogIspresent && prisonerIsAwake) {
+                console.log("annalyn sends her doggie pal to free the punk ass prisoner")
+        } else {
+            console.log("cant free shit.")
+        }
+       
+    }
+       
+       canFreePrisoner(knightIsAwake ,archerIsAwake ,petDogIspresent ,prisonerIsAwake);
+       canSignalPrisoner(archerIsAwake, prisonerIsAwake);
+       canSpy(knightIsAwake, archerIsAwake,prisonerIsAwake);
+       canExecuteFastAttack(knightIsAwake);
 });
